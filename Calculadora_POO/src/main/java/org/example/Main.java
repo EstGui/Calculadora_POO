0746236d5e8bb67fd98e81fa9b389633ed8dbf45
Guilherme Esteves <guilherme.o.esteves@gmail.com>
2024-05-26 09:22:00 -0300

    cmt_3

--- Calculadora_POO/src/main/java/org/example/Main.java
+++ Calculadora_POO/src/main/java/org/example/Main.java
@@ -7,3 +7,13 @@ public class Main {
         Menu.iniciar();
     }
 }
+
+/*
+    O programa funciona através de instruções;
+    Possui atributos e métodos privados, ou seja, o pilar de encapsulamento;
+    Funcionalidades do programa são separadas em classes e objetos diferentes;
+    Polimorfismo, onde as classes podem alterar seus comportamentos de maneira independentes.
+    Encapsulamento, onde é limitado o acesso à atributos e métodods;
+    Todas as entidades são geradas a partir de um "Molde" de objeto -> classe;
+    São utilizados métodos estáticos, para o uso de métodos sem a criação de instancias de uma classe utilitária;
+*/
